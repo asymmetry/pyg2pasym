ef1ea20ceda8676155dcb2153fd9775a92a96c37
Chao Gu <guchao.pku@gmail.com>
2017-04-25 22:23:34 -0400

    Improve the pre-calculation process

--- datatable.py
+++ datatable.py
@@ -12,11 +12,39 @@ except:
 for E in [2253.5, 1710.5, 1157.0, 3350.5]:
     xs = unpolxs(E)
     xs.calxs()
+    xs.save2pkl()
+    del xs
+
+    asym = asymmetry(E)
+    asym.calxs()
+    asym.calxsdf()
+    asym.save2pkl()
+    del asym
+
+    data = zload('data/asym_{}.pkl'.format(int(E)))
+    fortg12 = 'data/WQg12_{}.txt'.format(int(E))
+
+    W=data['W']
+    Q2=data['Q2GeV']
+    g1=data['g1']
+    g2=data['g2']
+
+    N=len(W)
+    g1[np.isnan(g1)]=0
+    g2[np.isnan(g2)]=0
+
+    with open(fortg12, 'w') as f:
+        for i in xrange(N):
+            f.write('{:7.2f} {:7.5f} {:12.9f} {:12.9f}\n'.format(W[i], Q2[i], g1[i], g2[i]))
+
+for E in [2253.5, 1710.5, 1157.0, 3350.5]:
+    xs = unpolxs(E)
+    xs.calxs()
     xs.calxsrad()
     xs.save2pkl()
     del xs
+
     asym = asymmetry(E)
-    #asym.data = zload('data/asym_{}.pkl'.format(int(E)))
     asym.calxs()
     asym.calxsrad()
     asym.calxsdf()
